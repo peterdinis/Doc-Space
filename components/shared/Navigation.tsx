6d9dfff52a1b8cd91919a9c7afdfcdfb2618f7bb
peterdinis <petter1@pobox.sk>
2025-07-11 10:05:26 +0200

    feat: Updating dark mode for app WIP

--- components/shared/Navigation.tsx
+++ components/shared/Navigation.tsx
@@ -6,7 +6,7 @@ import ThemeDropdown from "./ThemeDropdown";
 
 const Navigation: FC = () => {
 	return (
-		<header className="bg-white/80 backdrop-blur-md shadow-sm sticky top-0 z-50 animate-fade-in">
+		<header className="bg-white/80 dark:bg-background backdrop-blur-md shadow-sm sticky top-0 z-50 animate-fade-in">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex justify-between items-center h-16">
 					<div className="flex items-center animate-scale-in">

b171a7e5bd97d9bac1f2962525ce40430449406d
peterdinis <petter1@pobox.sk>
2025-07-15 18:06:03 +0200

    fix: Fixing bug with delete folder in sidebar

--- components/dashboard/AppSidebar.tsx
+++ components/dashboard/AppSidebar.tsx
@@ -207,16 +207,18 @@ export const AppSidebar = () => {
 													<>
 														<span className="flex-1">{folder.name}</span>
 														<X onClick={() => {
+															console.log("F", folder)
 															deleteFolder.mutate(String(folder?.id!))
 															toast({
 																title: "Folder was deleted",
 																duration: 2000,
 																className: "bg-green-800 text-white font-bold text-base leading-[130%]"
 															})
-															queryClient.refetchQueries({
+															queryClient.prefetchQuery({
 																queryKey: ["folders"]
 															})
 															window.location.reload()
+															
 														}} className="cursor-pointer text-red-700 dark:text-red-200" />
 													</>
 												)}
